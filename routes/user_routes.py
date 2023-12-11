ee92b1980329a61112943e7e35945ba3793a54a3
JexUgaz <a20191417@pucp.edu.pe>
2023-12-11 09:43:02 -0500

    nuevo

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -165,7 +165,8 @@ def setNewSlice():
 			port_vnc=find_available_portVNC()			
 			mac_addr=generar_mac()
 			init_VM(vlan_id=new_id_vlan,port_vnc=port_vnc,size_ram=vm.sizeRam,id_worker=0,path=vm.imagen[0]['path'],mac_addr=mac_addr)			
-			subprocess.run(f"ssh -f -N -L {port_vnc+5900}:localhost:{port_vnc+5900} ubuntu@{hosts[0]}",shell=True,text=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+			print(f"PUERTO: {port_vnc}")
+			subprocess.run(f"ssh -f -N -L {port_vnc+5900}:localhost:{port_vnc+5900} ubuntu@{hosts[0]} &",shell=True,text=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 			ports.append(port_vnc+5900)
 			ips_host.append(hosts[0])
 
