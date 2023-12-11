e0dd94aa1bfb2fffa9466912889faad4b3301b03
JexUgaz <a20191417@pucp.edu.pe>
2023-12-10 23:26:59 -0500

    Send Email corregido

--- config/helpers.py
+++ config/helpers.py
@@ -25,16 +25,19 @@ def generateNewPass():
 		if(not UserBDService.existeContrasena(contrasena)):
 			return contrasena
 
-def	sendMail(msg,receptor,subject):
-	conn=smtplib.SMTP(host= 'smtp.gmail.com',port=587)
-	conn.ehlo()
-	conn.starttls()
-	conn.login(user=EmailParams.emisor,password=EmailParams.passApp)
-	mensaje=MIMEText(msg,'html')
-	mensaje['FROM']=EmailParams.emisor
-	mensaje['To']=receptor
-	#mensaje['Cc'] = cc
-	mensaje['Subject']=subject
-	conn.sendmail(from_addr=EmailParams.emisor,to_addrs=receptor,msg=msg.as_string())
-	conn.quit()
-	print('Email enviado correctamente al destinatario!!')
+def sendMail(msg, receptor, subject):
+    conn = smtplib.SMTP(host='smtp.gmail.com', port=587)
+    conn.ehlo()
+    conn.starttls()
+    conn.login(user=EmailParams.emisor, password=EmailParams.passApp)
+
+    mensaje = MIMEText(msg, 'html', 'utf-8')  # Indicar el charset como 'utf-8'
+    mensaje['FROM'] = EmailParams.emisor
+    mensaje['To'] = receptor
+    mensaje['Subject'] = subject
+
+    # Convertir el mensaje a una cadena utf-8 antes de enviarlo
+    conn.sendmail(from_addr=EmailParams.emisor, to_addrs=receptor, msg=mensaje.as_string().encode('utf-8'))
+    conn.quit()
+
+    print('Email enviado correctamente al destinatario!!')
